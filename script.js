de87402eef57cec5222bb9cfb3b51391fc4c41a2
Kwabena Ampomah <kwabenaandampomah@gmail.com>
2019-12-11 23:53:26 +0000

    firstprojectagain

---
+++ script.js
@@ -0,0 +1,7 @@
+document.onload = function() {
+    var user = prompt("stop there is a virus in this webpage")
+}
+function question() {
+    var user = prompt("stop there is a virus in this webpage")
+}
+console.log(person);
